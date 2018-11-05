6e0cf4a960cd91b6839fc7f5d3c43a7fd3611f8f
JuLa35 <43664937+JuLa35@users.noreply.github.com>
2018-11-05 11:27:54 +0100

    Add files via upload

---
+++ Pdl/src/src/main/java/model/TestPdl.java
@@ -0,0 +1,14 @@
+package src.main.java.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestPdl {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
