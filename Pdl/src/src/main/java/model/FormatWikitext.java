7eae3dea10daa22ff335cdb9d7fc37d4faf904e8
robibin <muckenhirn.r@laposte.net>
2018-11-12 17:34:56 +0100

    Update FormatWikitext.java

--- Pdl/src/src/main/java/model/FormatWikitext.java
+++ Pdl/src/src/main/java/model/FormatWikitext.java
@@ -31,6 +31,23 @@ public class FormatWikitext
 		// TODO implement me
 		return null;
 	}
+	
+	public FormatWikitext wikisplit() {
+			String[] separateur = this.wikitext.split("title");
+			FormatWikitext result = new FormatWikitext(separateur[1]);
+			System.out.println("Texte à ajouter à l'URL:" + separateur[1]);
+			return result;
+			/*Url url = new Url("https://fr.wikipedia.org/w/index.php?title=" + result +"&action=edit&section=1");
+			return url;*/
+		}
+	
+	/*public FormatWikitext wikisplit2() {
+		String[] separateur = this.wikitext.split("title");
+		FormatWikitext result = new FormatWikitext(separateur[0]);
+		System.out.println(separateur[0]);
+		return result;
+	}*/
+
 
 }
 
