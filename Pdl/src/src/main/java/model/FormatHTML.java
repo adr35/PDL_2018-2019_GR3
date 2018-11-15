068947fe450e8e1ed5565eb5703a607dd5dfd5fc
JuLa35 <43664937+JuLa35@users.noreply.github.com>
2018-11-15 12:27:03 +0100

    Add files via upload

--- Pdl/src/src/main/java/model/FormatHTML.java
+++ Pdl/src/src/main/java/model/FormatHTML.java
@@ -1,6 +1,11 @@
 package src.main.java.model;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -18,7 +23,9 @@ public class FormatHTML
 	
 	
 	public String html;
-
+	
+	private String keep;
+	private int nbtab;
 	
 	public FormatHTML(){
 		this.html = new String();
@@ -26,6 +33,10 @@ public class FormatHTML
 	
 	public FormatHTML(String html){
 		this.html = html;
+	}
+	
+	public String getkeep() {
+		return keep;
 	}
 
 	/**
@@ -41,6 +52,7 @@ public class FormatHTML
 		return null;
 	}
 	
+	
 	//remplace le return par un String tab[] on aura tout les tableaux de la page
 	
 	/*début : wikitable
@@ -48,20 +60,24 @@ public class FormatHTML
 	 */
 	
 	public FormatHTML PremierSplit() {
-		String[] separateur = this.html.split("table class=\"");
+		keep = this.html;
+		FormatHTML clone = clone();
+		String[] separateur = clone.html.split("table class=\"wikitable");
 		/*ArrayList<String> pageWikitable = new ArrayList<String>();
 		    for(String clear : pageWikitable) {
 		        if (clear.startsWith("wikitable")) {
 		            pageWikitable.add(clear);
 		    }
 		}*/
+		nbtab = separateur.length -1;
 		FormatHTML result = new FormatHTML(separateur[1]);
 		//System.out.println(separateur[1]);
 		return result;
 	}
 	
 	public FormatHTML SecondSplit() {
-		String[] separateur = this.html.split("</tbody>");
+		FormatHTML html = PremierSplit();
+		String[] separateur = html.html.split("</tbody>");
 		FormatHTML result = new FormatHTML(separateur[0]);
 		//System.out.println(separateur[0]);
 		return result;
@@ -69,27 +85,68 @@ public class FormatHTML
 	
 	
 	public FormatHTML headSplit() {
-		String [] separateur = this.html.split("<tr>");
+		FormatHTML html = SecondSplit();
+		String [] separateur = html.html.split("<tr>");
 		FormatHTML result = new FormatHTML(separateur[1]);
 		//System.out.println(separateur[1]);
 		return result;		
 	}
 	
+	public int NombreCol() {
+		FormatHTML clone = headSplit();
+		int result =0;
+		String[] nbcol = clone.html.split("<th ");
+		result = nbcol.length -1;
+		return result;
+	}
+	
+	
 	public FormatHTML headParse() {
-		
-		Document doc = Jsoup.parse(this.html);
-		System.out.println(doc.html());
+		FormatHTML html = headSplit();
+		Document doc = Jsoup.parse(html.html);
+		//System.out.println(doc.html());
 		FormatHTML result = new FormatHTML();
 		Elements rows = doc.getAllElements();
-		for (Element row : rows ) {
-			
-			
-			//result.html += row.text();
-			System.out.println(row.text());
+		
+		/*for (Element row : rows ) {
+			result.html += row.text();
+			//System.out.println(row.text());
 
 		}
-		result.html = doc.html();
+		result.html = doc.html();*/
+		Element row = rows.first();
+		result.html = row.text();
+		//System.out.println(result.html);
 		return result;	
+	}
+	
+	public ProductionCSV headToCSV() {
+		int nbcol = NombreCol();
+		FormatHTML result = headParse();
+		String head = "";
+		int i = 0;
+		String[] test = result.html.split(" ");
+	for(String st : test) {
+			Pattern p = Pattern.compile("\\p{Upper}", Pattern.CASE_INSENSITIVE|Pattern.UNICODE_CASE);
+			Pattern p1 = Pattern.compile("\\p{Digit}", Pattern.CASE_INSENSITIVE|Pattern.UNICODE_CASE);
+			Pattern p2 = Pattern.compile("\\p{Punct}", Pattern.CASE_INSENSITIVE|Pattern.UNICODE_CASE);
+			if(i == 0) {
+				head += st;
+				i++;
+			}
+			else if(p2.matcher(st).find()){
+				head += " " + st;
+			}
+				
+			else if(p.matcher(st).find() || p1.matcher(st).find()) {
+				head += ("," + st);
+			}
+			else {
+				head += " " + st;
+			}
+		}
+		ProductionCSV csvhead = new ProductionCSV(head);
+		return csvhead;
 	}
 	
 
@@ -121,6 +178,11 @@ public class FormatHTML
 		result.html = doc.html();
 		writer.close();
 		return result;
+	}
+	
+	public FormatHTML clone() {
+		FormatHTML clone = new FormatHTML(this.html);
+		return clone;
 	}
 
 }
