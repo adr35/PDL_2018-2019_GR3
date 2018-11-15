48c8aed2c0ed392d12a31c9f8f1e5ee060e2fe54
robibin <muckenhirn.r@laposte.net>
2018-11-15 15:44:12 +0100

    Update Fichier.java

--- Pdl/src/src/main/java/model/Fichier.java
+++ Pdl/src/src/main/java/model/Fichier.java
@@ -10,60 +10,54 @@ import java.util.Set;
  */
 
 public class Fichier extends Url {
-	
+
 	public Set<Url> setUrl;
 
-	public Fichier(){
+	public Fichier() {
 		this.setUrl = new HashSet<Url>();
 	}
 	
+	public Fichier(Set<Url> setUrl) {
+		this.setUrl = setUrl;
+	}
+
 	public Fichier(Url urlUrl) {
 		this.setUrl = new HashSet<Url>();
 		this.setUrl.add(urlUrl);
 	}
-	
-	public Fichier(String urlString) {
+
+	/*public Fichier(String urlString) {
 		Url url = new Url(urlString);
 		this.setUrl = new HashSet<Url>();
 		this.setUrl.add(url);
-		}
+	}*/
+
 	
-	public Fichier(Set<Url> setUrl){
-		this.setUrl = setUrl;
-	}
+	/*
+	 * public Url receive() { Url url = new Url(""); url = url.getUrltest();
+	 * System.out.println(url); return url; }
+	 */
+
 	
-	
-	/*public Url receive() {
-		Url url = new Url("");
-		url = url.getUrltest();
-		System.out.println(url);
-		return url;
+	/*public boolean addString(String url) {
+		this.setUrl.add(new Url(url));
+		return true;
 	}*/
 	
-	public Fichier addUrl(Url url) {
-		Fichier fichier = new Fichier(url);
-		return fichier;
-	}
-		
-	public boolean addString(String url) {
-		//creer un nouveau fichier a chaque fois... XXX
-		this.setUrl.add(new Url(url));
+	public boolean addUrl(Url url) {
+		this.setUrl.add(url);
 		return true;
 	}
 
 	@Override
 	public String toString() {
-		String result= "Fichier [urlString= " + "\n";
-		for(Url url : this.setUrl) {
+		String result = "Fichier :" + "\n" + "[" + "\n";
+		for (Url url : this.setUrl) {
 			result += url.url + "\n";
 		}
 		result += "]";
 		return result;
-		// A tester avec les Url sous forme Url...
 	}
-
-	
-	
 
 }
 
