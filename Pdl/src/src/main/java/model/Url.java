6ec381a1d53d8d3aa3580bbe6c7945c1a92635d2
robibin <muckenhirn.r@laposte.net>
2018-11-21 16:11:16 +0100

    Update Url.java

--- Pdl/src/src/main/java/model/Url.java
+++ Pdl/src/src/main/java/model/Url.java
@@ -1,60 +1,81 @@
 package src.main.java.model;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
+
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 /**
- * <!-- begin-user-doc -->
- * <!--  end-user-doc  -->
+ * <!-- begin-user-doc --> <!-- end-user-doc -->
+ * 
  * @generated
  */
 
-public class Url
-{
+public class Url {
 
 	public String url;
+	public Url urltest;
+	//public URL Url;
+	
 
-	public Url(){
+	public Url() {
 		this.url = new String();
 	}
 	
-	public Url(String url){
+
+	public Url(String url) {
+		this.url = url;
+	}
+	
+	/*public Url(URL Url) {
+		this.Url = Url;
+	}*/
+	
+	public Url getUrltest() {
+		return this.urltest;
+	}
+	
+	public String getUrl() {
+		return this.url;
+	}
+	
+	public void setUrl(String url) {
 		this.url = url;
 	}
 
-		// A modifier....
-	public boolean isValidURL(String newUrl) {
-
-		Url url = null;
-
+		
+	public boolean isValidUrl() {
+		// int ch;
 		try {
-			url = new Url(newUrl);
-		} catch (MalformedURLException e) {
-			return false;
+			URL urlt = new URL(url);
+			HttpURLConnection connexion = (HttpURLConnection) urlt.openConnection();
+			InputStream flux = connexion.getInputStream();
+			System.out.println("Status de la connexion de " + urlt + ": " + connexion.getResponseMessage());
+			if (connexion.getResponseCode() == HttpURLConnection.HTTP_OK)
+				// while ((ch = flux.read()) != -1)
+				// System.out.print((char) ch); //Affiche le code HTML de la page
+				System.out.println("--> Url valide");
+			flux.close();
+			connexion.disconnect();
+			return true;
+		} catch (Exception e) {
+			System.out.println(e + "\n--> Url non valide");
 		}
-
-		try {
-			url.ToWikitext(newUrl);
-		} catch (URISyntaxException e) {
-			return false;
-		}
-
-		return true;
+		return false;
 	}
-	
+
 	/**
-	 * <!-- begin-user-doc -->
-	 * Renvoie le code HTML de l'url contenant le wikitext
-	 * <!--  end-user-doc  -->
+	 * <!-- begin-user-doc --> Renvoie le code HTML de l'url contenant le wikitext
+	 * <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 * @ordered
 	 */
@@ -65,31 +86,38 @@ public class Url
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * Renvoie le code HTML de l'url courant
-	 * <!--  end-user-doc  -->
+	 * <!-- begin-user-doc --> Renvoie le code HTML de l'url courant <!--
+	 * end-user-doc -->
+	 * 
 	 * @generated
 	 * @ordered
 	 */
-	
+
 	public FormatHTML ToHTML(Url parameter) {
 		// TODO implement me
 		return null;
 	}
+
 	
-	public String getUrl() {
-		return this.url;
-	}
-	
+
 	public String HTML() throws IOException {
 		Document doc = Jsoup.connect(getUrl()).get();
 		return doc.html();
 	}
+
+
+	/*@Override
+	public String toString() {
+		return "Url [Url=" + Url + "]";
+	}*/
+	
+	
+	
 	
 	/**
-	 * Utilisation de Servelet, un API, pour créer dynamiquement des données au sein
-	 * d'un serveur HTTP. Ces données sont le plus généralement présentées au format
-	 * HTML
+	 * Utilisation de Servelet, un API, pour crï¿½er dynamiquement des donnï¿½es au
+	 * sein d'un serveur HTTP. Ces donnï¿½es sont le plus gï¿½nï¿½ralement
+	 * prï¿½sentï¿½es au format HTML
 	 * 
 	 * @param requete
 	 * @param reponse
@@ -97,20 +125,14 @@ public class Url
 	 * @return
 	 * @throws Exception
 	 */
-	
-	/*URLs courants :
-	public boolean vérifURL(HttpServletRequest requete, HttpServletResponse reponse, Object handler) throws Exception {
-		String url = requete.getRequestURL().toString();
-		requete.setAttribute("urlBySpring", url);
 
-		return true;
-
-	}
-
-	public String recup(HttpServletRequest request) {
-		String url = request.getRequestURL();
-		System.out.println(url);
-	} */
+	/*
+	 * URLs courants : public boolean vï¿½rifURL(HttpServletRequest requete,
+	 * HttpServletResponse reponse, Object handler) throws Exception { String url =
+	 * requete.getRequestURL().toString(); requete.setAttribute("urlBySpring", url);
+	 * return true; } public String recup(HttpServletRequest request) { String url =
+	 * request.getRequestURL(); System.out.println(url); }
+	 */
 
 }
 
