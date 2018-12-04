0744384ab06ee707cee99afdfc895952f1aac760
Api1000 <43665694+Api1000@users.noreply.github.com>
2018-12-04 10:42:25 +0100

    Update TestPdl.java

--- Pdl/src/src/main/java/model/test/TestPdl.java
+++ Pdl/src/src/main/java/model/test/TestPdl.java
@@ -8,7 +8,7 @@ import src.main.java.model.Fichier;
 import src.main.java.model.Url;
 
 /**
- * Classe de tests pr�visionnels
+ * Classe de tests prévisionnels
  * @author Romiche
  *
  */
@@ -43,56 +43,66 @@ public class TestPdl extends Thread{
 }*/
 		 
 
-	/*@Test
-	public void testwiki() throws IOException {
-		String HTML = url.HTML();
-		String HTML2 = url2.HTML();
-		
-		FormatWikitext test = new FormatWikitext(HTML);
-		FormatWikitext test2 = new FormatWikitext(HTML2);
-		
-		//test = test.wikisplit();
-		//test = test.wikipremierSplit();
-		//test = test.wikisecondSplit();
-		//test = test.wikiheadSplit();
-		//System.out.println("");
-		test2 = test2.wikisplit();	
-		//System.out.print(test2.wikitext.toString());
-		Url url3 = new Url(test2.wikitext.toString());
-		System.out.print(url3.url.toString());
-		String HTML3 = url3.HTML();
-		//System.out.print(HTML3);
-		FormatWikitext test3 = new FormatWikitext(HTML3);
-		int nbtab = 0;
-		nbtab = test3.wikiCountTabs();
-		System.out.println("");
-		System.out.println("Nombre de tableaux sur cette page :" + nbtab);
-		//test3 = test3.wikiPremierSplit();
-		//System.out.print(test3.wikitext.toString());
-		//test3 = test3.wikiSecondSplit();
-		//System.out.print(test3.wikitext.toString());
-		test3 = test3.wikiFirstTab();
-		System.out.println("Tableau entier : ");
-		//System.out.println(test3.wikitext.toString());
-		//test3 = test3.printsplitTab();
-		//System.out.println(test3.wikitext.toString());
-		System.out.println("Head : ");
-		test3 = test3.wikiHeadSecondSplit(); //mettre en commentaire si wikiHeadParse() activï¿½
-		System.out.println(test3.wikitext.toString());
-		//test3 = test3.wikiHeadParse();
-		//System.out.println(test3.wikitext.toString());
-		//test3 = test3.wikiSplitHeadParse();
-		//System.out.println(test3.wikitext.toString());
-		//test2= test.wikiheadParse();
-		//test3 = test3.wikiRowSplit(); //mettre en commentaire si wikiRowParse() activï¿½
-		System.out.println("Lignes : ");
-		//System.out.println(test3.wikitext.toString());
-		test3 = test3.splitrow();
-		System.out.println(test3.wikitext.toString());
-		//test3 = test3.wikiSplitRowParse();
-		//System.out.println(test3.wikitext.toString());
-		assertEquals("", test2.wikitext, "");
-}*/
+	@Test
+			public void testwiki() throws IOException {
+				Url url = new Url("https://fr.wikipedia.org/w/index.php?title=Internationaux de France de tennis&action=edit");
+				Url url2 = new Url("https://en.wikipedia.org/wiki/Comparison_of_BitTorrent_tracker_software");
+				//https://en.wikipedia.org/wiki/Comparison_of_CRT,_LCD,_and_plasma
+				String HTML2 = url2.HTML();
+				
+				FormatWikitext test2 = new FormatWikitext(HTML2);
+				FormatWikitext lignes = new FormatWikitext(HTML2);
+				
+				//test = test.wikisplit();
+				//test = test.wikipremierSplit();
+				//test = test.wikisecondSplit();
+				//test = test.wikiheadSplit();
+				//System.out.println("");
+				//test2 = test2.wikisplit();
+				lignes = lignes.wikisplit();
+				//System.out.print(test2.wikitext.toString());
+				Url url3 = new Url(lignes.wikitext);
+				//System.out.println(url3.url.toString());
+				//System.out.println(lignes.wikitext.toString());
+				Url urlLignes = new Url(lignes.wikitext);
+				//System.out.print(url3.url.toString());
+				String HTML3 = url3.HTML();
+				String HTML4 = urlLignes.HTML();
+				//System.out.print(HTML3);
+				FormatWikitext test3 = new FormatWikitext(HTML3);
+				FormatWikitext lignes2 = new FormatWikitext(HTML4);
+				//ProductionCSV p = new ProductionCSV("coucou");
+				//p.generateCSV("cou", 1);
+				//test3 = test3.wikiPremierSplit();
+				//System.out.print(test3.wikitext.toString());
+			    test3 = test3.wikiSecondSplit();
+			    ProductionCSV prod = test3.headToCSV();
+				//lignes2 = lignes2.wikiSecondSplit();
+				//System.out.print(test3.wikitext.toString());
+				System.out.println("Tableau entier : ");
+				//System.out.println(test3.wikitext.toString());
+				//test3 = test3.printsplitRowTab();
+				//System.out.println(test3.wikitext.toString());
+				//test3 = test3.wikiFirstTab();
+				//System.out.println(test3.wikitext.toString());
+				System.out.println("\n" + "Head : ");
+				//test3 = test3.wikiHeadPremierSplit(); //mettre en commentaire si wikiHeadParse() activé
+			//	System.out.println(test3.wikitext.toString());
+				//test3 = test3.wikiHeadParse();
+				//test3 = test3.headToCSV();
+				//System.out.println(test3.wikitext.toString());
+				//test3 = test3.wikiSplitHeadParse();
+				//System.out.println(test3.wikitext.toString());
+				//test3= test.wikiHeadReplace();
+				 //mettre en commentaire si wikiRowParse() activé
+				System.out.println("Lignes : ");
+				//System.out.println(test3.wikitext.toString());
+				//lignes2 =lignes2.wikiRowPremierSplit();
+				//System.out.println(lignes2.wikitext.toString());
+				//lignes2 = lignes2.wikiRowParse();
+				//System.out.println(lignes2.wikitext.toString());
+			//	assertEquals("", test2.wikitext, "");
+}
 	
 	
 
