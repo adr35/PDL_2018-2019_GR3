33e2375951b0f885251be15ace26ff9bf090596e
Api1000 <43665694+Api1000@users.noreply.github.com>
2018-12-20 19:24:13 +0100

    Update TestPdl.java

--- Pdl/src/src/main/java/model/test/TestPdl.java
+++ Pdl/src/src/main/java/model/test/TestPdl.java
@@ -1,27 +1,48 @@
-package src.main.java.model.test;
-
+package src.main.java.model;
 import static org.junit.Assert.*;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
-import src.main.java.model.Fichier;
-import src.main.java.model.FormatWikitext;
-import src.main.java.model.ProductionCSV;
-import src.main.java.model.Url;
 
-/**
- * Classe de tests prévisionnels
- * @author Romiche
- *
- */
 
-@Test
+public class TestPdl {
+
+	final static File coupeDuMondeHTML = new File("html.txt");
+	public List <FormatWikitext> list_head = new ArrayList<FormatWikitext>();
+	//final static File presidentUSA = new File("HTML.txt");
+
+	/*@Test
+	public void test() {
+		fail("Not yet implemented");
+	}*/
+
+
+
+	/*@Test
+	public void testUrl() throws IOException {
+	 Url url = new Url("https://fr.wikipedia.org/wiki/Coupe du monde de football");
+	 String HTML = url.HTML();
+	 FormatHTML test = new FormatHTML(HTML);
+	 test = test.PremierSplit();
+	 test = test.SecondSplit();
+	 test = test.headSplit();
+	 test = test.headParse();
+	 //test = test.PremierParse();
+	 assertEquals("", test.html, "");
+
+}
+	 */
+	@Test
 	public void testwiki() throws IOException {
 		
-		Url url = new Url("https://en.wikipedia.org/wiki/Comparison_of_alphabetic_country_codes" );
+		Url url = new Url("https://en.wikipedia.org/wiki/Comparison_between_Ido_and_Interlingua" );
 
-		String HTML = url.HTML(); //Récupère le code HTML de la page
+		/*String HTML = url.HTML(); //Récupère le code HTML de la page
 		FormatWikitext newUrl = new FormatWikitext(HTML);
 		newUrl = newUrl.recupererURL();
 
@@ -30,104 +51,50 @@ import src.main.java.model.Url;
 		FormatWikitext newFormatWikitextHead = new FormatWikitext(WIKITEXT);
 		//newFormatWikitextHead.allTabs();
 		//newFormatWikitextHead = newFormatWikitextHead.getTableau();
-		newFormatWikitextHead.getHead1();
+		newFormatWikitextHead.ToCSV();;*/
 			//newFormatWikitextHead=newFormatWikitextHead.getHead();
 			/* -> Ici on recupère le head */
 
-			FormatWikitext newFormatWikitextRow = new FormatWikitext(WIKITEXT);
-			newFormatWikitextRow = newFormatWikitextRow.getRow();
+			//FormatWikitext newFormatWikitextRow = new FormatWikitext(WIKITEXT);
 			//newFormatWikitextRow = newFormatWikitextRow.wikiRowPremierSplit();
 			/* -> Ici on récupère les rows */
 
 			//newFormatWikitextHead.ToCSV(); // on créé un CSV head + rows
 		//System.out.println(newFormatWikitextRow.wikitext);
 		Fichier f = new Fichier();
+		f.productUrlsWikitext();
+		int i=0;
+		for(Url u : f.setUrl) {
+			String HTML = u.HTML(); //Récupère le code HTML de la page
+			FormatWikitext fw = new FormatWikitext(HTML);
+			/*newUrl = newUrl.recupererURL();
+
+			Url urlFormatWikitext = new Url(newUrl.wikitext); //creer un formatwikitext avec le code source contenant le wikitext
+			String WIKITEXT = urlFormatWikitext.HTML();
+			FormatWikitext newFormatWikitextHead = new FormatWikitext(WIKITEXT);*/
+			//newFormatWikitextHead.allTabs();
+			//newFormatWikitextHead = newFormatWikitextHead.getTableau();
+			fw.ToCSV();
+			System.out.println("page " + i + " en cours de traitement");
+			i++;
+		}
 
 	}
-public class TestPdl extends Thread{
 
-	final Url url = new Url("https://fr.wikipedia.org/wiki/Coupe du monde de football");
-	final Url url2 = new Url("https://fr.wikipedia.org/wiki/Internationaux de France de tennis");
-	final Url url3 = new Url("https://fr.wikipedia.org/wiki/Liste_des_pr%C3%A9sidents_des_%C3%89tats-Unis");
-	
-	final static File Wikiurls = new File("wikiurls.txt");
-	//final static File coupeDuMondeHTML = new File("html.txt");
-	//final static File presidentUSA = new File("HTML.txt");
-	
-	/*@Test
-	public void test() {
-		fail("Not yet implemented");
-	}*/
-
-	 
-		
-	 /*@Test
-		public void testUrl() throws IOException {
-		 String HTML = url.HTML();
-		 FormatHTML test = new FormatHTML(HTML);
-		 test = test.PremierSplit();
-		 test = test.SecondSplit();
-		 test = test.headSplit();
-		 test = test.headParse();
-		 //test = test.PremierParse();
-		 assertEquals("", test.html, "");
-		 
-}*/
-	
-			 @Test
-		 public void testHtml1() throws IOException, InterruptedException {
-			 Url url = new Url("https://fr.wikipedia.org/wiki/Coupe_du_monde_de_football");
-			 String HTML = url.HTML();
-			 FormatHTML ht = new FormatHTML(HTML);
-			 assertEquals(ht.getNbTab(),7);
-			 ht.ToCSV();
-			
-			// System.out.println(ht.html);
-			 
-			 
-		 }
-		 
-			 
-			 @Test
-			 public void testHtml2() throws IOException, InterruptedException {
-				 Url url = new Url("https://fr.wikipedia.org/wiki/Liste_des_présidents_des_États-Unis");
-				 String HTML = url.HTML();
-				 FormatHTML ht = new FormatHTML(HTML);
-				 assertEquals(ht.getNbTab(),1);
-				 ht.ToCSV();
-				
-				 
-				 
-			 }
-		 
-				@Test
-				public void testwiki() throws IOException {
-				Url url = new Url("https://en.wikipedia.org/wiki/Comparison_of_Linux_distributions" );
-
-				String HTML = url.HTML(); //Récupère le code HTML de la page
-				FormatWikitext newUrl = new FormatWikitext(HTML);
-				newUrl = newUrl.recupererURL();
-				
-				Url urlFormatWikitext = new Url(newUrl.wikitext); //creer un formatwikitext avec le code source contenant le wikitext
-				String WIKITEXT = urlFormatWikitext.HTML();
-				FormatWikitext newFormatWikitextHead = new FormatWikitext(WIKITEXT);
-				newFormatWikitextHead = newFormatWikitextHead.getTableau();
-				newFormatWikitextHead=newFormatWikitextHead.getHead();
-				/* -> Ici on recupère le head */
-				
-				FormatWikitext newFormatWikitextRow = new FormatWikitext(WIKITEXT);
-				newFormatWikitextRow = newFormatWikitextRow.getTableau();
-				newFormatWikitextRow = newFormatWikitextRow.wikiRowPremierSplit();
-				/* -> Ici on récupère les rows */
-				
-				FormatWikitext t = new FormatWikitext();
-				t.ToCSV(newFormatWikitextHead, newFormatWikitextRow); // on créé un CSV head + rows
-				//System.out.println(newFormatWikitextRow.wikitext);
-				Fichier f = new Fichier();
+	/*@Test 
+	 	public void testFichier() {
+		 	Url url = new Url("https://fr.wikipedia.org/wiki/Liste_des_pr%C3%A9sidents_des_%C3%89tats-Unis");
+		 	Url url2 = new Url("https://fr.wikipedia.org/wiki/Coupe du monde de football");
+		 	String newUrl = new String("https://fr.wikipedia.org/wiki/Liste_des_pr%C3%A9sidents_des_%C3%89tats-Unis");
+		 	String newUrl2 = new String("https://fr.wikipedia.org/wiki/Coupe du monde de football");
+		 	Fichier fichier = new Fichier();
+		 	fichier.addString(newUrl);
+			 fichier.addString(newUrl2);			 
+		 	//fichier = fichier.addString(newUrl2);
+		 	//fichier = fichier.addUrl(url2);
+			 System.out.println(fichier.toString());
+		 	//System.out.println(url2);
 
 }
-		
-	
-	
-
+	 */	
 }
