0f3e3eb57a3c9fb5f9d32ffc4c52da24f88b2622
JuLa35 <43664937+JuLa35@users.noreply.github.com>
2018-12-21 13:14:19 +0100

    Update Main.java

--- Pdl/src/src/main/java/model/Main.java
+++ Pdl/src/src/main/java/model/Main.java
@@ -4,15 +4,26 @@ import java.io.IOException;
 import src.main.java.model.*;
 
 public class Main {
+	
+	
 
 	public static void main(String[] args) throws IOException {
 		
+		System.out.println("-------------------------- DEBUT TRAITEMENT HTML --------------------------");
+		System.out.println("");
 		Fichier f = new Fichier();
 		f.productUrls();
 		f.FichierToHTML();
 		f = new Fichier();
+		System.out.println("");
+		System.out.println("-------------------------- FIN TRAITEMENT HTML --------------------------");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("-------------------------- DEBUT TRAITEMENT WIKITEXT --------------------------");
 		f.productUrlsWikitext();
 		f.FichierToWikitext();
+		System.out.println("-------------------------- FIN TRAITEMENT WIKITEXT --------------------------");
 	}
 
 }
